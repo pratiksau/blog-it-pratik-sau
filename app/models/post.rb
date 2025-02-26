1c6e7a129228c6dce34aeb477b24b7278f5a60bd
pratiksau <pratik.sau@bigbinary.com>
2025-02-26 11:56:52 +0530

    Added post controller and index action

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   MAXIMUM_TITLE_LENGTH = 125
   MAXIMUM_DESCRIPTION_LENGTH = 10000
 
-  validates :title, presence: true, length: { minimum: MAXIMUM_TITLE_LENGTH }
-  validates :description, presence: true, length: { minimum: MAXIMUM_DESCRIPTION_LENGTH }
+  validates :title, presence: true, length: { maximum: MAXIMUM_TITLE_LENGTH }
+  validates :description, presence: true, length: { maximum: MAXIMUM_DESCRIPTION_LENGTH }
   validates_inclusion_of :is_bloggable, in: [true, false]
 end
